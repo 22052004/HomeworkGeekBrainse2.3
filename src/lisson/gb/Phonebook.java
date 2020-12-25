11240e70acf31dc9b0668b31c3f8a42ccd2e6160
geekbrains <volkn139@gmail.com>
2020-12-25 16:08:01 +0300

    Initial commit

---
+++ src/lisson/gb/Phonebook.java
@@ -0,0 +1,17 @@
+package lisson.gb;
+import java.util.HashMap;
+import java.util.HashSet;
+public class Phonebook {
+    HashMap<String, HashSet<String>> pb;
+    public Phonebook() {
+        this.pb = new HashMap<>();
+    }
+    public void addContact(String name, String phone) {
+        HashSet<String> book = pb.getOrDefault(name, new HashSet<>());
+        book.add(phone);
+        pb.put(name, book);
+    }
+    public void findAndPrint(String name) {
+        System.out.println("Контакт " + name + " / Номер телефона " + pb.getOrDefault(name, new HashSet<>()));
+    }
+}
